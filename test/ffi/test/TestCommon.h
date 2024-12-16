d78d1cdae4786bf451bfb34314a01e27fa1cee9d
Urs Hanselmann <git@hanselmann.io>
2024-12-16 20:04:52 +0100

    add gtest

---
+++ test/ffi/test/TestCommon.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+// #include "absl/strings/str_cat.h"
+// #include "absl/strings/str_format.h"
+// #include "absl/strings/str_replace.h"
+// #include "absl/strings/str_join.h"
---
+++ test/ffi/test/test_ffi.cpp
@@ -0,0 +1,14 @@
+#include "common.h"
+
+#include "TestCommon.h"
+
+namespace {
+
+using ::testing::Eq;
+
+TEST(Test, PassingTest)
+{
+    EXPECT_TRUE(true);
+}
+
+} // namespace
